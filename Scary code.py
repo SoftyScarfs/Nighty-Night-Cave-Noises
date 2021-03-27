e08d7a6952eec36479008192d0cb51b51df7ff4b
SoftyScarfs <nathanwalker800@yahoo.com>
2021-03-27 18:49:54 -0500

    Cleand up some stuff

--- Scary code.py
+++ Scary code.py
@@ -4,13 +4,14 @@ import os
 import random
 
 
-
 # variables
 cwd = os.getcwd()
 print(cwd)
 SndNum = random.randint(0,4)
 
+
 # light value detection
+
 
 # play random sound sound
 playsound.playsound(str(cwd) + "\CaveSounds\CaveSnd" + str(SndNum) +".mp3")
