322d86ca91a6ba6e52cf20d00f2a1ce47fe93e52
SoftyScarfs <nathanwalker800@yahoo.com>
2021-03-27 19:01:38 -0500

    More sounds!

--- Scary code.py
+++ Scary code.py
@@ -1,17 +1,20 @@
 # imports
+# pip install paysound
 import playsound
 import os
 import random
 
 
 # variables
+# Find where the file is saved so it will run properly on any computer
 cwd = os.getcwd()
 print(cwd)
-SndNum = random.randint(0,4)
+
 
 
 # light value detection
 
 
 # play random sound sound
+SndNum = random.randint(0,12)
 playsound.playsound(str(cwd) + "\CaveSounds\CaveSnd" + str(SndNum) +".mp3")
