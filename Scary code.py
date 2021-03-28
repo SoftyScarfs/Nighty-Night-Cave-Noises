b2b17bfa6407159c1706cdcd151dd48e0a1d4179
SoftyScarfs <nathanwalker800@yahoo.com>
2021-03-28 01:11:24 -0500

    I added a hisogram. Now I just need to Isolate wich channel ima call "light" sence if the value is over 280 ish. and then play the cave sound if its not

--- Scary code.py
+++ Scary code.py
@@ -3,22 +3,43 @@
 # pip install paysound
 import playsound
 import random
+import cv2
+# pip install opencv-contrib-python
+import numpy as np
+import os
+import time
 
-# variables
+# variables / def
 
+def rescaleFrame(frame, scale=0.3):
+    width = int(frame.shape[1] * scale)
+    height = int(frame.shape[0] * scale)
+    dimensions = (width,height)
+    return cv2.resize(frame, dimensions, interpolation=cv2.INTER_AREA)
 # Find where the file is saved so it will run properly on any computer
 cwd = os.getcwd()
-
 
 
 # light value detection
 
 # get image
+cap = cv2.VideoCapture(0)
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+    # Our operations on the frame come here
+    frameRes = rescaleFrame(frame)
+    gray = cv2.cvtColor(frameRes, cv2.COLOR_BGR2GRAY)
 
-# value detect
-
-
-
+    # Display the resulting frame
+    cv2.imshow('frame',gray)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        capture.release()
+cv2.destroyAllWindows()
+# When everything done, release the capture
+cap.release()
+# value detect historgram thing I think
+gray_hist = cv2.calcHist([gray], [0], None, [256], [0,256] )
+print(gray_hist)
 # play random sound sound
 SndNum = random.randint(0,12)
 playsound.playsound(str(cwd) + "\CaveSounds\CaveSnd" + str(SndNum) +".mp3")
