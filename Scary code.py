a887f6d17c8d112ada94479c3b4311a973bcbd73
SoftyScarfs <nathanwalker800@yahoo.com>
2021-03-27 23:32:19 -0500

    Purge a bumch of broken an incompatible code.

--- Scary code.py
+++ Scary code.py
@@ -1,33 +1,24 @@
 # imports
+
 # pip install paysound
 import playsound
-import os
 import random
-import PIL
-from PIL import Image
-import cv2, time
 
 # variables
+
 # Find where the file is saved so it will run properly on any computer
 cwd = os.getcwd()
 
 
+
 # light value detection
 
-# video
-video=cv2.VideoCapture(0)
-check, frame = video.read()
+# get image
 
-filename = frame
-# value detection
-img = filename
-print(frame)
-colors = img.getpixel((320,240))
-print(colors)
-str(colors).split(", ")
-value = int(colors[0]) + int(colors[1]) + int(colors[2])
-value = value / 3
-print(value)
+# value detect
+
+
+
 # play random sound sound
 SndNum = random.randint(0,12)
 playsound.playsound(str(cwd) + "\CaveSounds\CaveSnd" + str(SndNum) +".mp3")
